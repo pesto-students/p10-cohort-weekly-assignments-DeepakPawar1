73638165135ab5a7e995f214111cf4759c4a1d15
Pawar, Deepak Atmaram <deepawar@deloitte.com>
2023-07-19 14:38:53 +0530

    Initial commit

---
+++ Week-12/todoapp/src/components/header.js
@@ -0,0 +1,14 @@
+import './header.css';
+
+function Header(){
+
+
+    return (
+
+        <div className="header">
+            ToDo Application
+        </div>
+    )
+};
+
+export default Header;
